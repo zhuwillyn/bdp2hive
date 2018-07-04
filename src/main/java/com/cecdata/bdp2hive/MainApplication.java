3bb83d502898ec3e79259b84051016b6c71e06ad
zhuweilin <zhuweilin@cecdat.com>
2018-07-04 11:42:02 +0800

    注释applicationContext.xml无用代码片段

--- src/main/java/com/cecdata/bdp2hive/MainApplication.java
+++ src/main/java/com/cecdata/bdp2hive/MainApplication.java
@@ -1,5 +1,7 @@
 package com.cecdata.bdp2hive;
 
+import org.apache.commons.cli.UnrecognizedOptionException;
+
 import java.lang.reflect.Method;
 
 /**
@@ -11,26 +13,30 @@ import java.lang.reflect.Method;
  */
 public class MainApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 通过java -jar jar文件 [hive|sqoop]命令执行相关的生成分区表或者生成sqoop脚本程序
         if (args.length < 1) {
             System.out.println("输入参数错误");
             System.exit(-1);
         }
-        // 判断进而利用反射执行不同类的启动方法
-        if ("hive".equals(args[0].toLowerCase())) {
-            Class<?> clazz = Class.forName("com.cecdata.bdp2hive.hive.GenerateHivePartitionTable");
-            Method method = clazz.getDeclaredMethod("main", String[].class);
-            method.setAccessible(true);
-            method.invoke(clazz.newInstance(), new Object[]{args});
-        } else if ("sqoop".equals(args[0].toLowerCase())) {
-            Class<?> clazz = Class.forName("com.cecdata.bdp2hive.sqoop.SqoopMainApplication");
-            Method method = clazz.getDeclaredMethod("main", String[].class);
-            method.setAccessible(true);
-            method.invoke(clazz.newInstance(), new Object[]{args});
-        } else {
-            System.out.println("please type [hive|sqoop] to start programs");
-            System.exit(-1);
+        try {
+            // 判断子命令进而利用反射执行不同类的启动方法
+            if ("hive".equals(args[0].toLowerCase())) {
+                Class<?> clazz = Class.forName("com.cecdata.bdp2hive.hive.GenerateHivePartitionTable");
+                Method method = clazz.getDeclaredMethod("main", String[].class);
+                method.setAccessible(true);
+                method.invoke(clazz.newInstance(), new Object[]{args});
+            } else if ("sqoop".equals(args[0].toLowerCase())) {
+                Class<?> clazz = Class.forName("com.cecdata.bdp2hive.sqoop.SqoopMainApplication");
+                Method method = clazz.getDeclaredMethod("main", String[].class);
+                method.setAccessible(true);
+                method.invoke(clazz.newInstance(), new Object[]{args});
+            } else {
+                System.out.println("please type [hive|sqoop] to start programs");
+                System.exit(-1);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 
